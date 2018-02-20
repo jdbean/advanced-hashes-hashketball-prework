c506ac06ccc0b0e5ed94d932d2b86ad9c3cfeca7
J.D. Bean <jonathandbean@gmail.com>
2018-02-20 23:58:22 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,105 @@
-# Write your code here!
-
+def game_hash
+  hash = {
+    home:
+      team_name: "Brooklyn Nets"
+      colors: ["Black", "White"]
+      players:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+    away: 
+      team_name:
+      colors:
+      players:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+        name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
+  }
+end
 
 
 
