1bfb4d007ebf5ed6ffe8e7008bb7da78b9f49a46
J.D. Bean <jonathandbean@gmail.com>
2018-02-21 00:07:58 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,7 +55,7 @@ def game_hash
           slam_dunks: 1
         }
       }
-    } 
+    }
     away: {
       team_name:
       colors:
@@ -107,5 +107,6 @@ def game_hash
           slam_dunks:
         }
       }
-  }   
+    }
+  }
 end
