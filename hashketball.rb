bc94aae79d5a5f90e5be10856d65cc1b94f93381
J.D. Bean <jonathandbean@gmail.com>
2018-02-21 00:26:58 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,3 +114,14 @@ def game_hash
     }
   }
 end
+
+def num_points_scored(player)
+  game_hash.each do |team, team_data|
+    team_data.each do |attribute, data|
+      if data = player
+        return data[points]
+      end
+    end
+  end
+end
+    
